f98b64a968292d356b2e2220ade2462c8aff2724
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2025-02-19 16:38:31 +0700

    Create Eagle.java

---
+++ LSP/Eagle.java
@@ -0,0 +1,10 @@
+public class Eagle implements Flyable {
+    @Override
+    public void walk(){
+        System.out.println("Eagle tok");
+    }
+    @Override
+    public void fly(){
+        System.out.println("Eagle prab prab");
+    }
+}
