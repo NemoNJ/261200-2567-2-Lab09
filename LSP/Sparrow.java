aa0c0fd0c5124f53a76cce57be537f3078500873
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2025-02-19 16:38:07 +0700

    Create Sparrow.java

---
+++ LSP/Sparrow.java
@@ -0,0 +1,10 @@
+public class Sparrow implements Flyable{
+    @Override
+    public void walk(){
+        System.out.println("Sparrow tok");
+    }
+    @Override
+    public void fly(){
+        System.out.println("Sparrow prab prab");
+    }
+}
