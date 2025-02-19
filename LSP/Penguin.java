f0b3c2dd3c6161d3a9c1f2e50f672d3e36fad012
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2025-02-19 16:37:24 +0700

    Create Penguin.java

---
+++ LSP/Penguin.java
@@ -0,0 +1,10 @@
+public class Penguin implements Swimmable {
+    @Override
+    public void walk(){
+        System.out.println("Penguin tok");
+    }
+    @Override
+    public void swim(){
+        System.out.println("Penguin Jomp jomp");
+    }
+}
