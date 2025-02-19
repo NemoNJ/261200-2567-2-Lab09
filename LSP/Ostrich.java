433d3a88525f106ead6ad9fd5c992960d3a72178
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2025-02-19 16:37:46 +0700

    Create Ostrich.java

---
+++ LSP/Ostrich.java
@@ -0,0 +1,10 @@
+public class Ostrich implements Runnable{
+    @Override
+    public void walk(){
+        System.out.println("Ostrich tok");
+    }
+    @Override
+    public void run(){
+        System.out.println("Ostrich tuk tuk");
+    }
+}
